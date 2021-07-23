e7517eb09e0e20175fde49cb118086cceccf3c9e
CZOLGOWUS <53970486+CZOLGOWUS@users.noreply.github.com>
2021-07-23 12:30:35 +0200

    fix animations a bit

--- proj_files/Assets/Scripts/Player/PlayerAnimationBehaviour.cs
+++ proj_files/Assets/Scripts/Player/PlayerAnimationBehaviour.cs
@@ -8,29 +8,38 @@ public class PlayerAnimationBehaviour : MonoBehaviour
     private Animator animator;
     private PlayerMovement movementScript;
 
-    public readonly bool isRunning;
+    public int animIsJumping;
+    private int animIsRunning;
+    private int animVelocity;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
         movementScript = GetComponent<PlayerMovement>();
+
+        animIsJumping = Animator.StringToHash("isJumping");
+        animIsRunning = Animator.StringToHash( "isRunning" );
+        animVelocity = Animator.StringToHash("Velocity");
+
     }
 
     private void FixedUpdate()
     {
 
         if( Mathf.Abs( rb.velocity.x ) > 0.1f )
-            animator.SetBool( "isRunning" ,true);
+            animator.SetBool( animIsRunning , true);
         else
-            animator.SetBool( "isRunning" , false );
+            animator.SetBool( animIsRunning , false );
 
 
         if( movementScript.IsJumping )
-            animator.SetBool( "isJumping" , true );
+            animator.SetBool( animIsJumping , true );
         else
-            animator.SetBool( "isJumping" , false );
+            animator.SetBool( animIsJumping , false );
 
+
+        animator.SetFloat( animVelocity , Mathf.Abs( rb.velocity.x / movementScript.maxSpeed ));
     }
 
 }
